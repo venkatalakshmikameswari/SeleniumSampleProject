2f1787937ac0beb36c627f5a9a9347b548f3fa5b
kameswari <bhagavatula.kameswari@gmail.com>
2021-07-30 17:00:21 -0700

    BrowserUtil added

--- src/main/java/helper/BrowserUtil.java
+++ src/main/java/helper/BrowserUtil.java
@@ -1,24 +1,33 @@
 package helper;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class BrowserUtil {
 	
 	static WebDriver driver;
-	
-	public static WebDriver browserFactoryMethod(String browser,String url) {
-		
-		if(browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("Webdriver.chrome.driver","C:\\Users\\meetr\\OneDrive\\Desktop\\New folder\\chromedriver_win32version_92\\chromedriver.exe");
-		    driver=new ChromeDriver();
-		    driver.get(url);
-		}
-		if(browser.equalsIgnoreCase("firefox")) {
+	public static WebDriver startBrowser(String browserName,String url)
+	{
+		if(browserName.equalsIgnoreCase("chrome"))
 			
+		{
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\meetr\\OneDrive\\Desktop\\New folder\\chromedriver_win32version_92\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.navigate().to(url);
 		}
-		return driver;
 		
+		if(browserName.equalsIgnoreCase("ie"))
+		{
+			throw new NotImplementedException("IE need to be implemented.");
+		
+		}
+		
+		return driver;
 	}
+	
 
 }
+
+
